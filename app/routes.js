42e9b09676323e5ae03c8dc9dc4ef4fac43adc16
grillopress <grillopress@gmail.com>
2019-11-01 12:30:07 +0000

    add subject to v3

--- app/routes.js
+++ app/routes.js
@@ -538,6 +538,36 @@ router.post('/app/send-a-message/v3/message-you-sent', function (req, res) {
 
 });
 
+router.post('/app/send-a-message/v4/message-you-sent', function (req, res) {
+
+  // get
+
+  req.session.data.msgTrue = "True";
+  req.session.data.msgDate = "today";
+  req.session.data.msgTime = moment().format("h:mma");
+  req.session.data.msgBody = req.body.msgBody;
+  req.session.data.msgSender = req.session.data.sender
+  req.session.data.msgFrom = req.session.data.msgTo;
+  // req.session.data.msgTo = "True";
+  // id
+  // from
+  // req.body.checkMobile
+
+  // then build query string for messaging/v1/message
+  // /{{ PROTOTYPEURL }}/your-message?msgTo=Woodrow GP Practice
+  // message?id=1&from=Leanna Leveille&sent=Yesterday&time=4.55pm&body=Please arrange for an appointment with a nurse for a blood test
+  var query = "/app/send-a-message/v4/message?id=2&"
+              + "from=" + req.session.data.msgFrom + "&"
+              + "sent=" + req.session.data.msgDate + "&"
+              + "time=" + req.session.data.msgTime + "&"
+              + "body=" + req.session.data.msgBody + "&"
+              + "subject" + req.session.data.msgSubject + "&"
+              + "to=" + "True" + "&"
+              + "messageRead2=" + "read" + "&"
+  res.redirect(query)
+
+});
+
 router.post('/app/send-a-message/v3/send-a-message-radio', function (req, res) {
   let answer = req.body.urgentAdvice;
 
@@ -547,6 +577,30 @@ router.post('/app/send-a-message/v3/send-a-message-radio', function (req, res) {
 
   } else {
     res.redirect('/app/send-a-message/v3/select-who')
+  }
+});
+
+router.post('/app/send-a-message/v4/send-a-message-radio', function (req, res) {
+  let answer = req.body.urgentAdvice;
+
+  if (answer === 'yes') {
+
+    res.redirect('/app/send-a-message/v4/contact-your-gp')
+
+  } else {
+    res.redirect('/app/send-a-message/v4/select-who')
+  }
+});
+
+router.post('/app/send-a-message/v4/message-subject-radio', function (req, res) {
+  let answer = req.body.subject;
+
+  if (answer === 'Other') {
+
+    res.redirect('/app/send-a-message/v4/message-subject')
+
+  } else {
+    res.redirect('/app/send-a-message/v4/send-a-message-radio')
   }
 });
 
