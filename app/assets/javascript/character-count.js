228adb4f39c87a52fdd119d5ef37e7d5dadb84ad
Samantha Saw <samantha.saw@nhs.net>
2019-11-14 16:29:36 +0000

    Updated character count and content on confirmed page

--- app/assets/javascript/character-count.js
+++ app/assets/javascript/character-count.js
@@ -19,3 +19,26 @@ $(document).ready(function() {
 
     });
 });
+
+
+$(document).ready(function() {
+    var text_max = 1000;
+    $('#textarea_feedback_1000').html('You have ' + text_max + ' characters remaining');
+
+    $('textarea').keyup(function() {
+        var text_length = $('textarea').val().length;
+        var text_remaining = text_max - text_length;
+
+        if (text_length > text_max) {
+
+          var text_reverse_negative =- text_remaining;
+
+          $('#textarea_feedback_1000').html('You have ' + text_reverse_negative + ' characters too many').addClass('nhsuk-error-message').removeClass('nhsuk-hint');
+
+        } else {
+
+          $('#textarea_feedback_1000').html('You have ' + text_remaining + ' characters remaining').removeClass('nhsuk-error-message').addClass('nhsuk-hint');
+        }
+
+    });
+});
