32b4a7b76f54aeac7e7e17909ffe32d870f8faf5
grillopress <grillopress@gmail.com>
2019-11-15 00:01:20 +0000

    Turn character count into function

--- app/assets/javascript/character-count.js
+++ app/assets/javascript/character-count.js
@@ -1,44 +1,21 @@
-$(document).ready(function() {
-    var text_max = 400;
-    $('#textarea_feedback').html('You have ' + text_max + ' characters remaining');
+function characterCount(id, textMax) {
+  var text_max = textMax;
+  $(id).html('You have ' + text_max + ' characters remaining');
 
-    $('textarea').keyup(function() {
-        var text_length = $('textarea').val().length;
-        var text_remaining = text_max - text_length;
+  $('textarea').keyup(function() {
+      var text_length = $('textarea').val().length;
+      var text_remaining = text_max - text_length;
 
-        if (text_length > text_max) {
+      if (text_length > text_max) {
 
-          var text_reverse_negative =- text_remaining;
+        var text_reverse_negative =- text_remaining;
 
-          $('#textarea_feedback').html('You have ' + text_reverse_negative + ' characters too many').addClass('nhsuk-error-message').removeClass('nhsuk-hint');
+        $(id).html('You have ' + text_reverse_negative + ' characters too many').addClass('nhsuk-error-message').removeClass('nhsuk-hint');
 
-        } else {
+      } else {
 
-          $('#textarea_feedback').html('You have ' + text_remaining + ' characters remaining').removeClass('nhsuk-error-message').addClass('nhsuk-hint');
-        }
+        $(id).html('You have ' + text_remaining + ' characters remaining').removeClass('nhsuk-error-message').addClass('nhsuk-hint');
+      }
 
-    });
-});
-
-
-$(document).ready(function() {
-    var text_max = 1000;
-    $('#textarea_feedback_1000').html('You have ' + text_max + ' characters remaining');
-
-    $('textarea').keyup(function() {
-        var text_length = $('textarea').val().length;
-        var text_remaining = text_max - text_length;
-
-        if (text_length > text_max) {
-
-          var text_reverse_negative =- text_remaining;
-
-          $('#textarea_feedback_1000').html('You have ' + text_reverse_negative + ' characters too many').addClass('nhsuk-error-message').removeClass('nhsuk-hint');
-
-        } else {
-
-          $('#textarea_feedback_1000').html('You have ' + text_remaining + ' characters remaining').removeClass('nhsuk-error-message').addClass('nhsuk-hint');
-        }
-
-    });
-});
+  });
+}
