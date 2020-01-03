ff7664d657266a4051a193003c6257e42eebeaf3
grillopress <grillopress@gmail.com>
2020-01-03 11:17:20 +0000

    reinstate forced https

--- app.js
+++ app.js
@@ -111,11 +111,11 @@ useHttps = useHttps.toLowerCase()
 // Force HTTPS on production. Do this before using basicAuth to avoid
 // asking for username/password twice (for `http`, then `https`).
 //
-// var isSecure = (env === 'production' && useHttps === 'true')
-// if (isSecure) {
-//  app.use(utils.forceHttps)
-//  app.set('trust proxy', 1) // needed for secure cookies on heroku
-// }
+var isSecure = (env === 'production' && useHttps === 'true')
+if (isSecure) {
+app.use(utils.forceHttps)
+app.set('trust proxy', 1) // needed for secure cookies on heroku
+}
 
 // initial checks
 checkFiles()
