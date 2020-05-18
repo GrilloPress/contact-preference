2e1fa788503d26473527655d517e23712aeb66aa
grillopress <grillopress@gmail.com>
2020-05-18 14:53:48 +0100

    add v5 messaging section to include how repling will work

--- app/routes/messaging.js
+++ app/routes/messaging.js
@@ -191,6 +191,80 @@ router.post('/app/send-a-message/v4/file/check-file', function (req, res) {
 
 
 
+
+
+
+router.post('/app/send-a-message/v5/message-you-sent', function (req, res) {
+
+  // get
+
+  req.session.data.msgTrue = "True";
+  req.session.data.msgDate = "today";
+  req.session.data.msgTime = moment().format("h:mma");
+  req.session.data.msgBody = req.body.msgBody;
+  req.session.data.msgSender = req.session.data.sender
+  req.session.data.msgFrom = req.session.data.msgTo;
+  // req.session.data.msgTo = "True";
+  // id
+  // from
+  // req.body.checkMobile
+
+  // then build query string for messaging/v1/message
+  // /{{ PROTOTYPEURL }}/your-message?msgTo=Woodrow GP Practice
+  // message?id=1&from=Leanna Leveille&sent=Yesterday&time=4.55pm&body=Please arrange for an appointment with a nurse for a blood test
+  var query = "/app/send-a-message/v5/message?id=2&"
+              + "from=" + req.session.data.msgFrom + "&"
+              + "sent=" + req.session.data.msgDate + "&"
+              + "time=" + req.session.data.msgTime + "&"
+              + "body=" + req.session.data.msgBody + "&"
+              + "subject" + req.session.data.msgSubject + "&"
+              + "to=" + "True" + "&"
+              + "messageRead2=" + "read" + "&"
+  res.redirect(query)
+
+});
+
+router.post('/app/send-a-message/v5/send-a-message-radio', function (req, res) {
+  let answer = req.body.urgentAdvice;
+
+  if (answer === 'yes') {
+
+    res.redirect('/app/send-a-message/v5/contact-your-gp')
+
+  } else {
+    res.redirect('/app/send-a-message/v5/select-who')
+  }
+});
+
+router.post('/app/send-a-message/v5/message-subject-radio', function (req, res) {
+  let answer = req.body.subject;
+
+  if (answer === 'Other') {
+
+    res.redirect('/app/send-a-message/v5/message-subject')
+
+  } else {
+    res.redirect('/app/send-a-message/v5/send-a-message-radio')
+  }
+});
+
+router.post('/app/send-a-message/v5/file/check-file', function (req, res) {
+  let answer = req.body.checkFile;
+
+  if (answer === 'yes') {
+
+    req.session.data.fileAdded = "true";
+    res.redirect('/app/send-a-message/v5/your-message')
+
+  } else {
+    // req.session.data.fileAdded = "false";
+    res.redirect('/app/send-a-message/v5/file/add-file')
+  }
+});
+
+
+
+
 // SEND A PATIENT ROUTES
 
 router.post('/send-a-patient-a-message/v2/create/sender/from', function (req, res) {
