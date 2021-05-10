697ddd160905783e5e365f55f2dee23c527d152b
“samanthasaw” <samantha.saw@nhs.net>
2021-05-10 14:37:15 +0100

    Added covid status v3

--- app/routes.js
+++ app/routes.js
@@ -334,6 +334,33 @@ router.post('/app/covid-status/status-service-v2/accept-terms', function (req, r
 });
 
 
+router.post('/app/covid-status/status-service-v3/accept-terms', function (req, res) {
+
+  let acceptCovidTermsThree = req.body.acceptCovidTermsThree;
+
+  if (acceptCovidTermsThree) {
+
+    res.redirect('/app/covid-status/travel-hub?acceptCovidTermsThreeError=')
+
+  } else {
+
+    var errorURL = "";
+
+    if (acceptCovidTermsThree) {
+
+      errorURL += "acceptCovidTermsThreeError=&"
+
+    } else {
+       errorURL += "acceptCovidTermsThreeError=True&"
+    }
+
+    res.redirect('/app/covid-status/status-service-v3/accept-terms?' + errorURL )
+  }
+
+});
+
+
+
 
 
 //////   IMPORTANT  /////////////////////////////////////////////////////////////////
