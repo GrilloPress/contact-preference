0382f5e37f6df22ec214e67ca22587872068b591
“samanthasaw” <samantha.saw@nhs.net>
2021-08-19 17:58:41 +0100

    Journey and content updates for round 3

--- app/routes/contact-details.js
+++ app/routes/contact-details.js
@@ -64,6 +64,54 @@ router.post('/app/contact-details/round-2/scenario-3/which-email', function (req
 
 
 
+router.post('/app/contact-details/round-3/scenario-1/which-email', function (req, res) {
+  let answer = req.body.whichEmail;
+
+  if (answer === 'differentEmail') {
+
+    res.redirect('/app/contact-details/round-3/scenario-1/enter-new-email')
+
+  } else {
+    res.redirect('/app/contact-details/round-3/scenario-1/email-updated')
+  }
+
+
+});
+
+
+
+router.post('/app/contact-details/round-3/scenario-2/which-phone', function (req, res) {
+  let answer = req.body.whichPhone;
+
+  if (answer === 'differentPhone') {
+
+    res.redirect('/app/contact-details/round-3/scenario-2/enter-password')
+
+  } else {
+    res.redirect('/app/contact-details/round-3/scenario-2/phone-updated')
+  }
+
+
+});
+
+
+
+router.post('/app/contact-details/round-3/scenario-3/which-email', function (req, res) {
+  let answer = req.body.whichEmail;
+
+  if (answer === 'gp') {
+
+    res.redirect('/app/contact-details/round-3/scenario-3/enter-new-gp-email')
+
+  } else {
+    res.redirect('/app/contact-details/round-3/scenario-3/enter-new-login-email')
+  }
+
+
+});
+
+
+
 
 
 
